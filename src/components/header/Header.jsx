deca0f2d21533f474bdecfd83b5d7c671aae4281
sunshine-web-coder <webdev4gaming@gmail.com>
2024-02-20 19:17:51 +0100

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css"
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img className="Logo" src="https://i.imgur.com/sxP0bPs.png" alt="logo" />
+      <div className="hamburger">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/userDB.jsx
@@ -0,0 +1,11 @@
+const users = [
+  {
+    id: 1,
+    name: "Robert",
+    cardName: "Robert",
+    username: "Robert",
+    password: "R1u2b3y4",
+    amount: "50,000,000.00",
+  },
+];
+export default users;
